5d64fc47a86b058d54d776a1fdf495c6b7774293
MIguel Torres <mtorres913@gmail.com>
2023-05-25 09:59:24 -0500

    created get route for random act

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -6,7 +6,16 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  const queryText = `SELECT acts from acts ORDER BY RANDOM() LIMIT 1`;
+  pool
+  .query(queryText)
+  .then((result) => {
+    res.send(result.rows);
+  })
+  .catch((error) => {
+    console.log(`Error on query ${error}`);
+    res.sendStatus(500);
+  })
 });
 
 /**
