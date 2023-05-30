63b0c1ffb0216dbda5ade488cebd2a993e0b3f79
MIguel Torres <mtorres913@gmail.com>
2023-05-30 13:10:38 -0500

    created delete route in acts.router.js

--- server/routes/acts.router.js
+++ server/routes/acts.router.js
@@ -36,5 +36,15 @@ router.post('/', (req, res) => {
     })
 });
 
-router.delet
+router.delete('/:id', (req,res) => {
+  console.log(req.params.id);
+  const deleteIndex = Number(req.params.id);
+  let queryText = 'DELETE FROM "acts" WHERE "id" = $1;';
+  pool.query(queryText, [deleteIndex]).then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log(`Error in DELETE ${error}`)
+    res.sendStatus(500);
+  })
+})
 module.exports = router;
